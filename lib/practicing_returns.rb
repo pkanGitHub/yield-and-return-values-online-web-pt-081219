c3bdbf4adba780b134edc0ec16a97141b8657e41
Phyllis Kan <pyk9q7@gmail.com>
2019-08-19 01:17:30 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,6 +3,7 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
+    binding.pry
     yield(array[i])
     i += 1
   end
@@ -10,3 +11,4 @@ end
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
